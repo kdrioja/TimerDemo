4d0dd9b1d77b8b80d3f05901c06cc60b51932370
Kenia Rioja-Naranjo <kdrioja@gmail.com>
2019-10-20 23:05:40 -0700

    Done working with demo

--- app/src/main/java/com/example/timerdemo/MainActivity.java
+++ app/src/main/java/com/example/timerdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.timerdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         This is one way we can do a timer
+
+
+        // handler allows us to do some timing functions
+        final Handler handler = new Handler();
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Hey it's us", "A second has passed by");
+
+                handler.postDelayed(this, 1000); //refers to time in ms
+            }
+        };
+
+        handler.post(run);
+         */
+
+        /**
+         Prefered way of instructor on how to do a timer
+         */
+        new CountDownTimer(10000, 1000) {
+
+            public void onTick(long msUntilDone) {
+                Log.i("Seconds left", String.valueOf(msUntilDone / 1000));
+            }
+
+            public void onFinish() {
+                Log.i("We're done", "No more countdown");
+            }
+        }.start();
     }
 }
